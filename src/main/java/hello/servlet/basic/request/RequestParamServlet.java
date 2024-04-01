3e2d363c6e62900f9d5c35b867d1506ef04a262a
wawawaiii <qhgud2563@gmail.com>
2024-04-01 22:26:28 +0900

    ParamServlet

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회]- start");
+        request.getParameterNames().asIterator().forEachRemaining(paramNames->System.out.println(paramNames+"="+request.getParameter(paramNames)));
+        System.out.println("[전체 파라미터 조회]- end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("usesrname="+username);
+        System.out.println("age="+age);
+    }
+}
