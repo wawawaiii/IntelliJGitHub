32709b88f178749f0d73ca42662b88fcce428fe5
wawawaiii <qhgud2563@gmail.com>
2024-05-02 19:33:16 +0900

    ParamServlet

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,6 +2,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
@@ -9,3 +11,4 @@ public class ServletApplication {
         SpringApplication.run(ServletApplication.class, args);
     }
 }
+
