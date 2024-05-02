c0db5134bdbaef23f8fb3f31397dca8cb782500c
wawawaiii <qhgud2563@gmail.com>
2024-05-02 10:19:54 +0900

    ParamServlet

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "mvcMemverFormServlet")
-public class MvcMemverFormServlet extends HttpServlet {
+public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String viewPath = "/WEB-INF/Views/new-form.jsp";
