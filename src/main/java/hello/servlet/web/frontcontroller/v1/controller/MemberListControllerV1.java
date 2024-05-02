2ca3f687eb42136b140a1e97b6898fedd8171964
wawawaiii <qhgud2563@gmail.com>
2024-05-02 09:59:52 +0900

    ParamServlet

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -9,20 +9,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
-public class MemberSaveControllerV1 implements ControllerV1 {
+public class MemberListControllerV1 implements ControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
-        Member member = new Member(username, age);
-        System.out.println("member = " + member);
-        memberRepository.save(member);
-        //Model에 데이터를 보관한다.
-        request.setAttribute("member", member);
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
